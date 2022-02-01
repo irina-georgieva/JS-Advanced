004d2c110bc032ccfaa9612e3b8cfac29eacce1f
Irina Georgieva <georgieva2490@gmail.com>
2022-02-01 22:40:17 +0100

    Added new solution

--- 10 DOM Introduction Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
+++ 10 DOM Introduction Exercise/04. JS-Advanced-DOM-Introduction-Exercise-Resources/04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/08. Generate Report/report.js
@@ -1,3 +1,28 @@
 function generateReport() {
-    //TODO
+    let resultEl = document.getElementById('output');
+    let rows = Array.from(document.querySelectorAll('tbody tr'));
+    let checkedBox = Array.from(document.querySelectorAll('input[type=checkbox]'));
+
+    let result = [];
+
+    for (let i = 0; i < rows.length; i++) {
+        let info = {};
+
+        for (let j = 0; j < checkedBox.length; j++) {
+            if (checkedBox[j].checked) {
+                let section = checkedBox[j].name;
+
+                let text = rows[i].textContent.split("\n").map(x => x.trim());
+                text.shift();
+                text.pop();
+
+                info[section] = text[j];
+            }
+        }
+
+        result.push(info);
+    }
+
+    resultEl.value = JSON.stringify(result);
+
 }
